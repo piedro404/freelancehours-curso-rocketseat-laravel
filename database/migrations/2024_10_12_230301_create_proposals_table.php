8ac08cd399a8cf82485f9b79eaa022930c5d826d
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-10-12 20:10:17 -0300

    feat: :seedling: Add Proposals

---
+++ database/migrations/2024_10_12_230301_create_proposals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Project;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->unsignedTinyInteger('hours');
+            $table->foreignIdFor(Project::class)->constrained('projects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proposals');
+    }
+};
